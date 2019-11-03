de89c5c7b3dc3d3b1947c2784aa92cbd9dd5ac7c
Sherzod Makhmudov <sherkka0204@gmail.com>
2019-11-03 10:51:38 -0800

    MessagingUICompleted

---
+++ gearUP/Controller/MessageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MessageViewController.swift
+//  gearUP
+//
+//  Created by Sherzod Makhmudov on 10/24/19.
+//  Copyright © 2019 com.SherzodMakhmudov. All rights reserved.
+//
+
+import Foundation
---
+++ gearUP/View/MessageCell.swift
@@ -0,0 +1,9 @@
+//
+//  MessageCell.swift
+//  gearUP
+//
+//  Created by Sherzod Makhmudov on 11/2/19.
+//  Copyright © 2019 com.SherzodMakhmudov. All rights reserved.
+//
+
+import Foundation
